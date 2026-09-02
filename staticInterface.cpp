4ba2276b512b64e2c400138b6eb55104e7530927
Michal <raptorad@gmail.com>
2026-09-02 16:56:58 +0200

    Create staticInterface.cpp

---
+++ staticInterface.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+//*******************************************************
+// Using concepts to create a static interface
+// requires C++ 2020 or newer
+//*******************************************************
+
+//************************************************
+// Concept Printable which requires print method
+//************************************************
+template<class T>
+concept Printable = requires(T a)
+{
+    a.print();
+};
+
+class ConcreteA
+{
+public:
+    void print() const
+    {
+        std::cout<<"print a\n";
+    }
+};
+
+class ConcreteB
+{
+public:
+    int print() const
+    {
+        std::cout<<"print b\n";
+        return 0;
+    }
+};
+
+
+//***************************************************************************
+// Function which uses concept. Can use all classes which has a print method
+//***************************************************************************
+template<Printable P>
+void universalPrint(const P& Pr)
+{
+    Pr.print();
+}
+
+int main()
+{
+    ConcreteA objA;
+    ConcreteB objB;
+    universalPrint(objA);
+    universalPrint(objB);
+    return 0;
+}
